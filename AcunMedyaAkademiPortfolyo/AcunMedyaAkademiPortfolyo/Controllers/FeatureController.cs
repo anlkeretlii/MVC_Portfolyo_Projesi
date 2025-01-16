8b982e164192ea16f6ad2b4c4c4ef0a63c89dfa8
Anıl Engin Keretli <116742352+anlkeretlii@users.noreply.github.com>
2025-01-16 20:39:32 +0300

    deneme 3

---
+++ AcunMedyaAkademiPortfolyo/AcunMedyaAkademiPortfolyo/Controllers/FeatureController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcunMedyaAkademiPortfolyo.Models;
+
+namespace AcunMedyaAkademiPortfolyo.Controllers
+{
+    public class FeatureController : Controller
+    {
+        DbProductAcunMedyaEntities db = new DbProductAcunMedyaEntities();
+        // GET: Feature
+        public ActionResult Index()
+        {
+            var values = db.Feature.ToList();
+            return View(values);
+        }
+        [HttpGet]
+        public ActionResult CreateFeature()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult CreateFeature(Feature p)
+        {
+            db.Feature.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult DeleteFeature(int id)
+        {
+            var value = db.Feature.Find(id);
+            db.Feature.Remove(value);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult UpdateFeature(int id)
+        {
+            var value = db.Feature.Find(id);
+            return View(value);
+        }
+        [HttpPost]
+        public ActionResult UpdateFeature(Feature p)
+        {
+            var value = db.Feature.Find(p.FeatureId);
+            value.FeatureTitle = p.FeatureTitle;
+            value.FeatureSubtitle = p.FeatureSubtitle;
+            value.FeatureImageUrl = p.FeatureImageUrl;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ AcunMedyaAkademiPortfolyo/AcunMedyaAkademiPortfolyo/Controllers/StatisticController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AcunMedyaAkademiPortfolyo.Controllers;
+using AcunMedyaAkademiPortfolyo.Models;
+
+
+
+namespace AcunMedyaAkademiPortfolyo.Controllers
+{
+    public class StatisticController : Controller
+    {
+        DbProductAcunMedyaEntities db = new DbProductAcunMedyaEntities();
+        // GET: Statistic
+        public ActionResult Index()
+        {
+            ViewBag.categoryCount= db.Category.Count();
+            ViewBag.projectCount= db.tblProject.Count();
+            ViewBag.skillCount= db.Skill.Count();
+            ViewBag.skillAvgValue = db.Skill.Average(X => X.Value);
+            ViewBag.lastSkillTitleName = db.Getlastskilltitle1().FirstOrDefault();
+            ViewBag.mvcCategoryProjectCount = db.tblProject.Where(x => x.ProjectCategory == 4).Count();
+
+            return View();
+        }
+    }
+}
